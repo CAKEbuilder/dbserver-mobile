1868fc5a2d777400b2975e821f4415c97ab6b3c5
CAKEbuilder <tecuserx@gmail.com>
2016-06-15 17:12:00 -0400

    check the os architecture
    this check will help inform the user of which file on the WMF 5 page to download

--- psversion.ps1
+++ psversion.ps1
@@ -1,11 +1,33 @@
+# get the OS architecture
+if ([Environment]::Is64BitOperatingSystem) {
+    $arch = 'x64'
+    } else {
+    $arch = 'x86'
+}
 
+
+
+# get the version of Powershell
 $psver = $PSVersionTable.PSVersion.Major
 if ($psver -lt 4 ) {
 
-    Write-Host 'Powershell version (' $psver ') is too low'
+    Write-Host "Powershell version:"$psver
+    Write-Host "OS:"$arch
+    Write-Host ""
+    Write-Host "this version is too low"
+    Write-Host ""
+    Write-Host "download Windows Management Framework 5 (the link is on the github page)"
+    Write-Host ""
 
 } else {
+    
+    Write-Host "Powershell version:"$psver
+    Write-Host "OS:"$arch
+    Write-Host ""
+    Write-Host "this version will work"
+    Write-Host ""
+    Write-Host "you're all set!"
+    Write-Host ""
 
-    Write-Host 'your version of Powershell (' $psver ') will work. no further action is needed.'
     }
 
