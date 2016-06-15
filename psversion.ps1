71b7c17a56624ad50ca7631e733b200bd74aa5ac
CAKEbuilder <tecuserx@gmail.com>
2016-06-15 18:03:46 -0400

    Win 8 and 8.1 support

--- psversion.ps1
+++ psversion.ps1
@@ -3,6 +3,19 @@
 
 #definitions
 $url = 'https://www.microsoft.com/en-us/download/details.aspx?id=50395'
+$psver = $PSVersionTable.PSVersion.Major
+
+# get the OS version (make this prettier you lazy shit)
+if ([System.Environment]::OSVersion.Version.Major -eq 7) {
+    $osver = '7'
+    }
+# '-like' here because I'm not sure how 8.1 shows up even as a Major object...
+if ([System.Environment]::OSVersion.Version.Major -like 8) {
+    $osver = '8'
+    }
+if ([System.Environment]::OSVersion.Version.Major -eq 10) {
+    $osver = '10'
+    }
 
 
 # get the OS architecture
@@ -12,21 +25,45 @@ if ([Environment]::Is64BitOperatingSystem) {
     $arch = 'x86'
 }
 
+
+
+# set the correct filename to download for this user
 if ($arch -eq 'x64') {
-    $file2download = 'Win7AndW2K8R2-KB3134760-x64.msu'
-    } else {
-    $file2download = 'Win7-KB3134760-x86.msu'
+    if ($osver -eq '7') {
+        $file2download = 'Win7AndW2K8R2-KB3134760-x64.msu'
+    }
+    if ($osver -like '8') {
+        $file2download = 'Win8.1AndW2K12R2-KB3134758-x64.msu'
+    }
+}
+if ($arch -eq 'x86') {
+    if ($osver -eq '7') {
+        $file2download = 'Win7-KB3134760-x86.msu'
+    }
+    if ($osver -like '8') {
+        $file2download = 'Win8.1-KB3134758-x86.msu'
+    }
+}
+
+# error if we can't determine the architecture
+if ($arch -ne 'x86' -and $arch -ne 'x64') {
+    Write-Host 'cannot get your OS architecture'
+    Write-Host 'please contact CAKEbuildercs@gmail.com'
+    pause
+    exit
 }
 
 
 
-# get the version of Powershell
-$psver = $PSVersionTable.PSVersion.Major
+
+# inform the user on what they need to do, if anything
 if ($psver -lt 4 ) {
 
+    # open the download page using the default browser
     Start-Process $url
     Write-Host "Powershell version:"$psver
-    Write-Host "OS Arch:"$arch
+    Write-Host "OS version:"$osver
+    Write-Host "OS arch:"$arch
     Write-Host ""
     Write-Host "this version is too low, download Powershell 5"
     Write-Host ""
@@ -36,7 +73,8 @@ if ($psver -lt 4 ) {
 } else {
     
     Write-Host "Powershell version:"$psver
-    Write-Host "OS Arch:"$arch
+    Write-Host "OS version:"$osver
+    Write-Host "OS arch:"$arch
     Write-Host ""
     Write-Host "this version will work"
     Write-Host ""
