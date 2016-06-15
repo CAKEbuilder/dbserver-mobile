42bea1ebcec0b9a54ccbfebefed31abf1b0757a5
CAKEbuilder <tecuserx@gmail.com>
2016-06-15 18:48:02 -0400

    Update psversion.ps1

--- psversion.ps1
+++ psversion.ps1
@@ -3,7 +3,8 @@
 
 #definitions
 $url = 'https://www.microsoft.com/en-us/download/details.aspx?id=50395'
-$psver = $PSVersionTable.PSVersion.Major
+#$psver = $PSVersionTable.PSVersion.Major
+$psver=4
 
 # get the OS architecture
 if ([Environment]::Is64BitOperatingSystem) {
@@ -33,20 +34,20 @@ if ($psver -lt 5 ) {
     Write-Host "Powershell version:"$psver
     Write-Host "OS arch:"$arch
     Write-Host ""
-    Write-Host "this version is too low, download Powershell 5"
+    Write-Host "This version is too low, download Powershell 5"
     Write-Host ""
     if ($arch -eq 'x64') {
-        Write-Host 'if you have Windows 7, download - Win7AndW2K8R2-KB3134760-x64.msu'
+        Write-Host 'if you have Windows 7, download the 2nd file (Win7AndW2K8R2-KB3134760-x64.msu)'
         Write-Host ''
-        Write-Host 'if you have Windows 8/8.1, download - Win8.1AndW2K12R2-KB3134758-x64.msu'        
+        Write-Host 'if you have Windows 8/8.1, download the 4th file (Win8.1AndW2K12R2-KB3134758-x64.msu)'
     }
     if ($arch -eq 'x86') {
-        Write-Host 'if you have Windows 7, download - Win7-KB3134760-x86.msu'
+        Write-Host 'if you have Windows 7, download the 3rd file (Win7-KB3134760-x86.msu)'
         Write-Host ''
-        Write-Host 'if you have Windows 8/8.1, download - Win8.1-KB3134758-x86.msu'
+        Write-Host 'if you have Windows 8/8.1, download the 5th file (Win8.1-KB3134758-x86.msu)'
     }
     Write-Host ''
-    Write-Host "press enter and I will bring you to the download page"
+    Write-Host "when you are ready, press Enter and I will bring you to the download page"
     Write-Host ''
     Write-Host ''
     pause
@@ -58,10 +59,11 @@ if ($psver -lt 5 ) {
     Write-Host "Powershell version:"$psver
     Write-Host "OS arch:"$arch
     Write-Host ""
-    Write-Host "this version will work"
+    Write-Host "This version will work"
     Write-Host ""
     Write-Host "you're all set!"
     Write-Host ""
+    pause
 
     }
 
