9df53ffe226e97c0a8a8930cd0648a2d3ad62235
CAKEbuilder <tecuserx@gmail.com>
2016-06-15 17:39:21 -0400

    open download link in default browser if needed
    just need to account for relaying the appropriate Windows 8 file name if Win8 is the user's OS

--- psversion.ps1
+++ psversion.ps1
@@ -1,8 +1,21 @@
+# note: Win10 comes with Powershell v5 by default
+# https://4sysops.com/archives/powershell-versions-and-their-windows-version/#powershell-and-windows-versions
+
+#definitions
+$url = 'https://www.microsoft.com/en-us/download/details.aspx?id=50395'
+
+
 # get the OS architecture
 if ([Environment]::Is64BitOperatingSystem) {
     $arch = 'x64'
     } else {
     $arch = 'x86'
+}
+
+if ($arch -eq 'x64') {
+    $file2download = 'Win7AndW2K8R2-KB3134760-x64.msu'
+    } else {
+    $file2download = 'Win7-KB3134760-x86.msu'
 }
 
 
@@ -11,12 +24,13 @@ if ([Environment]::Is64BitOperatingSystem) {
 $psver = $PSVersionTable.PSVersion.Major
 if ($psver -lt 4 ) {
 
+    Start-Process $url
     Write-Host "Powershell version:"$psver
     Write-Host "OS:"$arch
     Write-Host ""
-    Write-Host "this version is too low"
+    Write-Host "this version is too low, download Powershell 5"
     Write-Host ""
-    Write-Host "download Windows Management Framework 5 (the link is on the github page)"
+    Write-Host "I will open your browser to the download page. The only file you need to download and install is -"$file2download
     Write-Host ""
 
 } else {
