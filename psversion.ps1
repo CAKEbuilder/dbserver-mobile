ecee51ee1427121a53f77302e337b15a9b2952ad
CAKEbuilder <tecuserx@gmail.com>
2016-06-15 18:28:43 -0400

    removed os check, annoying without v5...

--- psversion.ps1
+++ psversion.ps1
@@ -5,19 +5,6 @@
 $url = 'https://www.microsoft.com/en-us/download/details.aspx?id=50395'
 $psver = $PSVersionTable.PSVersion.Major
 
-# get the OS version (make this prettier you lazy shit)
-if ([System.Environment]::OSVersion.Version.Major -eq 7) {
-    $osver = '7'
-    }
-# '-like' here because I'm not sure how 8.1 shows up even as a Major object...
-if ([System.Environment]::OSVersion.Version.Major -like '*8*') {
-    $osver = '8/8.1'
-    }
-if ([System.Environment]::OSVersion.Version.Major -eq 10) {
-    $osver = '10'
-    }
-
-
 # get the OS architecture
 if ([Environment]::Is64BitOperatingSystem) {
     $arch = 'x64'
@@ -27,23 +14,7 @@ if ([Environment]::Is64BitOperatingSystem) {
 
 
 
-# set the correct filename to download for this user
-if ($arch -eq 'x64') {
-    if ($osver -eq '7') {
-        $file2download = 'Win7AndW2K8R2-KB3134760-x64.msu'
-    }
-    if ($osver -like '*8*') {
-        $file2download = 'Win8.1AndW2K12R2-KB3134758-x64.msu'
-    }
-}
-if ($arch -eq 'x86') {
-    if ($osver -eq '7') {
-        $file2download = 'Win7-KB3134760-x86.msu'
-    }
-    if ($osver -like '*8*') {
-        $file2download = 'Win8.1-KB3134758-x86.msu'
-    }
-}
+
 
 # error if we can't determine the architecture
 if ($arch -ne 'x86' -and $arch -ne 'x64') {
@@ -59,21 +30,32 @@ if ($arch -ne 'x86' -and $arch -ne 'x64') {
 # inform the user on what they need to do, if anything
 if ($psver -lt 5 ) {
 
-    # open the download page using the default browser
-    Start-Process $url
     Write-Host "Powershell version:"$psver
-    Write-Host "OS version:"$osver
     Write-Host "OS arch:"$arch
     Write-Host ""
     Write-Host "this version is too low, download Powershell 5"
     Write-Host ""
-    Write-Host "I will open your browser to the download page. The only file you need to download and install is -"$file2download
-    Write-Host ""
+    if ($arch -eq 'x64') {
+        Write-Host 'if you have Windows 7, download - Win7AndW2K8R2-KB3134760-x64.msu'
+        Write-Host ''
+        Write-Host 'if you have Windows 8/8.1, download - Win8.1AndW2K12R2-KB3134758-x64.msu'        
+    }
+    if ($arch -eq 'x86') {
+        Write-Host 'if you have Windows 7, download - Win7-KB3134760-x86.msu'
+        Write-Host ''
+        Write-Host 'if you have Windows 8/8.1, download - Win8.1-KB3134758-x86.msu'
+    }
+    Write-Host ''
+    Write-Host "press enter and I will bring you to the download page"
+    Write-Host ''
+    Write-Host ''
+    pause
+    # open the download page using the default browser
+    Start-Process $url
 
 } else {
     
     Write-Host "Powershell version:"$psver
-    Write-Host "OS version:"$osver
     Write-Host "OS arch:"$arch
     Write-Host ""
     Write-Host "this version will work"
